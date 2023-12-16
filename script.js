0237917dcc500defc406b8b79648d1289c676d90
Adrian Tut <adriantutuianu01@gmail.com>
2023-12-16 00:39:21 +0000

    Getting random number from an array

--- script.js
+++ script.js
@@ -104,7 +104,11 @@ function getPasswordOptions() {
 }
 
 // Function for getting a random element from an array
-function getRandom(arr) {}
+function getRandom(arr) {
+  return arr[Math.floor(Math.random() * arr.length)]; //getting random number from arr
+}
+
+getRandom(upperCasedCharacters);
 
 // Function to generate password with user input
 function generatePassword() {
